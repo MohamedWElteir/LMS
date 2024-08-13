3ff2c71dfb7b17eae2f0c1762772b3988a2be306
Mohamed Wael <hfhybujj@gmail.com>
2024-08-13 20:47:52 +0300

    First commit

---
+++ src/main/java/com/library/management/maids/model/Patron.java
@@ -0,0 +1,18 @@
+package com.library.management.maids.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "patrons", uniqueConstraints = {@UniqueConstraint(columnNames = {"name", "contactInfo"})})
+public class Patron {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String contactInfo;
+}
